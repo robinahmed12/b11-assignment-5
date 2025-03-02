30b86c15fb9bb914156f4232dd0fb8b24fcf9c48
robinahmed12 <shawonahmed0294@gmail.com>
2025-03-02 11:47:52 +0600

    added bg random color change functionality

--- Script/script.js
+++ Script/script.js
@@ -78,3 +78,28 @@ const day = daysOfWeek[currentDate.getDay()];
 const date = currentDate.toLocaleDateString();
 
 document.querySelector("#local-date").innerText = `${day}, ${date}`;
+
+
+// bg change random
+
+function getRandomColor() {
+  let letter = '0123456789ABCDEF';
+  let color = '#';
+  
+  
+  for (let i = 0; i < 6; i++) {
+
+    color+= letter[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+
+const colorChangeBtn = document.getElementById('change-color-btn');
+
+colorChangeBtn.addEventListener('click' ,
+  function () {
+   document.body.style.background = getRandomColor();
+  }
+)
